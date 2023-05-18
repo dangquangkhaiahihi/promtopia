bc0dc60aab40a45c6d9669f6b2e6da9a83fc64ac
khaiahihi <dang.quang.khai2610@gmail.com>
2023-05-18 22:42:39 +0700

    strike 1 - consistent coding -  know a bit about setting up tailwindcss and create UI with tailwindcss

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,19 @@
+import Feed from "@components/Feed";
+
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">Pet project Pet projectPet projectPet projectPet projectPet projectPet project</p>
+
+      {/* Feed */}
+      <Feed/>
+    </section>
   )
 }
 
