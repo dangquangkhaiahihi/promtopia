49aa6fdcb495738f1d952ba66593c8cdc39eac80
khaiahihi <dang.quang.khai2610@gmail.com>
2024-05-13 11:22:33 +0700

    layout new logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -126,7 +126,8 @@ const RootLayout = ({ children, meta }) => {
                         </button>
                         <div className="absolute left-[50%] [transition:transform_0.3s_ease-in-out,opacity_0.2s_ease-in-out] lg:static lg:translate-x-0 translate-x-[-50%]">
                         <Link href="/">
-                            <svg
+                            <img src="/assets/icons/logo.svg" title="Logo" alt="Logo" style={{height: '40px'}}/>
+                            {/* <svg
                                 role="img"
                                 width={82}
                                 preserveAspectRatio="xMinYMin meet"
@@ -135,157 +136,13 @@ const RootLayout = ({ children, meta }) => {
                                 xmlns="http://www.w3.org/2000/svg"
                                 xmlnsXlink="http://www.w3.org/1999/xlink"
                             >
-                            <title>TIME Logo</title>
-                            <path
-                                fill="#e90606"
-                                d="M3 2.36h23.67v5.4h-.43l.05-3.46h-9.35v24.36h2.7v.8H10.4v-.8h2.5V4.3H3.45v3.46H3v-5.4zM27.8 2.36h9.3v.75h-2.65v25.56h2.64v.8h-9.3v-.8h2.7V3.1h-2.7v-.74zM45.3 2.3h1.78l8.7 20.9 8.64-20.9h1.73L71 28.66h2.44v.8h-9.6v-.8h2.47l-3-16.85-7.68 17.67h-1.8l-7.6-17.66-3.2 16.86h2.33l-.05.8h-7.07l.05-.8h2.16L45.3 2.3zM74.25 2.36h20.47l.05 5.35h-.37l-.17-3.3-13.4-.15v10.7h11.4l.27-2.5h.5c-.12 2.28-.12 4.5 0 6.76h-.5l-.27-2.58h-11.4V27.7h13.9l.04-3.68h.38v5.45h-20.9v-.76l2.7-.04V3.16l-2.7-.05v-.74z"
-                            />
-                            </svg>
+                                <title>TIME Logo</title>
+                                <path
+                                    fill="#e90606"
+                                    d="M3 2.36h23.67v5.4h-.43l.05-3.46h-9.35v24.36h2.7v.8H10.4v-.8h2.5V4.3H3.45v3.46H3v-5.4zM27.8 2.36h9.3v.75h-2.65v25.56h2.64v.8h-9.3v-.8h2.7V3.1h-2.7v-.74zM45.3 2.3h1.78l8.7 20.9 8.64-20.9h1.73L71 28.66h2.44v.8h-9.6v-.8h2.47l-3-16.85-7.68 17.67h-1.8l-7.6-17.66-3.2 16.86h2.33l-.05.8h-7.07l.05-.8h2.16L45.3 2.3zM74.25 2.36h20.47l.05 5.35h-.37l-.17-3.3-13.4-.15v10.7h11.4l.27-2.5h.5c-.12 2.28-.12 4.5 0 6.76h-.5l-.27-2.58h-11.4V27.7h13.9l.04-3.68h.38v5.45h-20.9v-.76l2.7-.04V3.16l-2.7-.05v-.74z"
+                                />
+                            </svg> */}
                         </Link>
-                        </div>
-                    </div>
-                    <div className="flex items-center">
-                        <div className="absolute left-[50%] top-0 hidden h-[100%] -translate-x-1/2 items-center [transition:transform_0.3s_ease-in-out,opacity_0.2s_ease-in-out] lg:flex">
-                        <a
-                            href="#"
-                            className="whitespace-nowrap font-roboto-condensed text-17px font-extralight uppercase text-time-black antialiased hover:text-time-red flex items-center gap-1.5"
-                        >
-                            <svg
-                            role="img"
-                            aria-hidden="true"
-                            width={21}
-                            height={21}
-                            viewBox="0 0 21 21"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlnsXlink="http://www.w3.org/1999/xlink"
-                            >
-                            <g
-                                stroke="none"
-                                strokeWidth={1}
-                                fill="none"
-                                fillRule="evenodd"
-                            >
-                                <g transform="translate(-852.000000, -102.000000)">
-                                <g transform="translate(303.000000, 102.000000)">
-                                    <g transform="translate(549.000000, 0.000000)">
-                                    <g>
-                                        <circle
-                                        fill="rgba(0,0,0,0.95)"
-                                        cx="10.5"
-                                        cy="10.5"
-                                        r="10.5"
-                                        />
-                                        <g
-                                        strokeLinecap="round"
-                                        transform="translate(10.735157, 10.632020) rotate(-9.000000) translate(-10.735157, -10.632020) translate(2.235157, 5.263599)"
-                                        stroke="#ffffff"
-                                        strokeWidth="0.8"
-                                        >
-                                        <polyline
-                                            transform="translate(5.830469, 5.598202) rotate(9.000000) translate(-5.830469, -5.598202) "
-                                            points="1.35678474 1.1245181 6.0975063 10.0718865 10.3041532 6.03597723"
-                                        />
-                                        <polyline
-                                            transform="translate(6.784174, 5.296309) rotate(9.000000) translate(-6.784174, -5.296309) "
-                                            points="1.41575288 1.26999362 10.8187298 6.27031557 12.152595 9.3226252"
-                                        />
-                                        <line
-                                            x1="8.92892695"
-                                            y1="8.12445114"
-                                            x2="11.6131375"
-                                            y2="9.91392482"
-                                            transform="translate(10.271032, 9.019188) rotate(9.000000) translate(-10.271032, -9.019188) "
-                                        />
-                                        <polyline
-                                            transform="translate(8.901535, 3.366942) rotate(9.000000) translate(-8.901535, -3.366942) "
-                                            points="1.74364069 1.57746822 16.0594302 2.31288991 12.2457603 5.15641559"
-                                        />
-                                        <polyline
-                                            transform="translate(6.784174, 5.296309) rotate(9.000000) translate(-6.784174, -5.296309) "
-                                            points="1.41575288 1.26999362 12.152595 4.86711944 12.149983 9.3226252"
-                                        />
-                                        </g>
-                                    </g>
-                                    </g>
-                                </g>
-                                </g>
-                            </g>
-                            </svg>
-                            Sign Up for Our Entertainment Newsletter
-                        </a>
-                        </div>
-                        <a
-                        href="#"
-                        className="rounded-3px bg-time-red px-3 py-2.5 font-roboto-condensed text-base font-bold uppercase tracking-1px text-white antialiased [transition:transform_0.3s_ease-in-out,opacity_0.2s_ease-in-out]"
-                        >
-                        <span className="lg:hidden">Subscribe</span>
-                        <span className="hidden lg:inline">Subscribe</span>
-                        </a>
-                        <div className="relative flex items-center">
-                        <button
-                            type="button"
-                            aria-label="Search Button"
-                            className="ml-1.5 hidden px-1.5 [transition:transform_0.3s_ease-in-out] lg:block"
-                        >
-                            <svg
-                            role="img"
-                            aria-hidden="true"
-                            width={32}
-                            height={32}
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                            >
-                            <path
-                                fill="#282f2f"
-                                d="M14.1 14.751a5.53 5.53 0 1 1 .651-.651L19 18.348l-.652.652-4.248-4.249zm-3.557.4a4.608 4.608 0 1 0 0-9.216 4.608 4.608 0 0 0 0 9.215z"
-                            />
-                            </svg>
-                        </button>
-                        <div className="absolute left-[2.375rem] flex h-[1.875rem] w-[26rem] items-center justify-start overflow-hidden [transition:transform_0.3s_ease-in-out,opacity_0.2s_ease-in-out] lg:justify-between pointer-events-none opacity-0">
-                            <div className="pointer-events-none opacity-0 [transition:transform_0.3s_ease-in-out,opacity_0.2s_ease-in-out] lg:hidden">
-                            <svg
-                                role="img"
-                                aria-hidden="true"
-                                width={32}
-                                height={32}
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                fill="#282f2f"
-                                d="M14.1 14.751a5.53 5.53 0 1 1 .651-.651L19 18.348l-.652.652-4.248-4.249zm-3.557.4a4.608 4.608 0 1 0 0-9.216 4.608 4.608 0 0 0 0 9.215z"
-                                />
-                            </svg>
-                            </div>
-                            <form>
-                            <input
-                                type="text"
-                                placeholder="Search..."
-                                className="relative w-[13.5rem] bg-transparent py-1 pl-2 font-pt-serif italic text-time-gray-1 antialiased outline-none lg:w-96"
-                                name="q"
-                                defaultValue=""
-                            />
-                            </form>
-                            <button
-                            type="button"
-                            className="relative top-[0.0625rem] hidden px-3 lg:block"
-                            aria-label="Close Search Button"
-                            >
-                            <svg
-                                role="img"
-                                aria-hidden="true"
-                                width={14}
-                                height={14}
-                                viewBox="0 0 14 14"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                fill="#282f2f"
-                                d="M14 1.41L12.59 0 7 5.59 1.41 0 0 1.41 5.59 7 0 12.59 1.41 14 7 8.41 12.59 14 14 12.59 8.41 7z"
-                                />
-                            </svg>
-                            </button>
-                        </div>
                         </div>
                     </div>
                     </nav>
@@ -854,7 +711,8 @@ const RootLayout = ({ children, meta }) => {
                     href="#"
                     aria-label="Logo"
                 >
-                    <svg
+                    <img src="/assets/icons/logo.svg" title="Logo" alt="Logo" style={{height: '40px'}}/>
+                    {/* <svg
                         role="img"
                         // itemProp="logo"
                         width="90px"
@@ -870,7 +728,7 @@ const RootLayout = ({ children, meta }) => {
                         fill="#e90606"
                         d="M3 2.36h23.67v5.4h-.43l.05-3.46h-9.35v24.36h2.7v.8H10.4v-.8h2.5V4.3H3.45v3.46H3v-5.4zM27.8 2.36h9.3v.75h-2.65v25.56h2.64v.8h-9.3v-.8h2.7V3.1h-2.7v-.74zM45.3 2.3h1.78l8.7 20.9 8.64-20.9h1.73L71 28.66h2.44v.8h-9.6v-.8h2.47l-3-16.85-7.68 17.67h-1.8l-7.6-17.66-3.2 16.86h2.33l-.05.8h-7.07l.05-.8h2.16L45.3 2.3zM74.25 2.36h20.47l.05 5.35h-.37l-.17-3.3-13.4-.15v10.7h11.4l.27-2.5h.5c-.12 2.28-.12 4.5 0 6.76h-.5l-.27-2.58h-11.4V27.7h13.9l.04-3.68h.38v5.45h-20.9v-.76l2.7-.04V3.16l-2.7-.05v-.74z"
                     />
-                    </svg>
+                    </svg> */}
                 </a>
                 </div>
                 <div
